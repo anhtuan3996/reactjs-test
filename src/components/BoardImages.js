30939cc33735defcab163a54028414e56ba518d5
tuan <anhtuan3996@gmail.com>
2022-12-21 23:07:37 +0700

    handle action

--- src/components/BoardImages.js
+++ src/components/BoardImages.js
@@ -4,18 +4,16 @@ import ImageService from "../services/image.service";
 import EventBus from "../common/EventBus";
 
 const BoardImages = () => {
+    const [name, setName] = React.useState("");
+
     const [content, setContent] = useState([]);
+
+    const [selectedId, setSelectedId] = useState('');
 
     useEffect(() => {
         ImageService.getImages().then(
             (response) => {
-                const content =  response.data.map((res) => {
-                    return (
-                        <div className="col-lg-2 col-md-6 mb-4" key={res.id} >
-                            <img className="card-img-top" src={res.download_url} alt="Card image cap" />
-                        </div>
-                    )
-                });
+                const content =  response.data
                 setContent(content);
             },
             (error) => {
@@ -35,10 +33,78 @@ const BoardImages = () => {
         );
     }, []);
 
+    React.useEffect(() => {
+        setTimeout(() => {
+            ImageService.getImages({ name }).then(
+                (response) => {
+                    const content =  response.data
+                    setContent(content);
+                },
+                (error) => {
+                    const _content =
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString();
+
+                    setContent(_content);
+
+                    if (error.response && error.response.status === 401) {
+                        EventBus.dispatch("logout");
+                    }
+                }
+            );
+        }, 2000)
+    }, [name])
+
+    const handleChooseItem = id => {
+        // TODO: Call API
+        if (id == selectedId) {
+            setSelectedId('')
+        }else {
+            setSelectedId(id)
+        }
+    };
+
+    const handleSkip = () => {
+        // TODO: Call API
+        console.log('handleSkip')
+    };
+
+    const handleSave = () => {
+        // TODO: Call API
+        console.log('handleSave')
+    };
+
     return (
-        <div className="row d-flex justify-content-center">
-            {content}
-        </div>
+        <>
+            <div className="row">
+                <div className="col-3">
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Item name" onChange={(event) => setName(event.target.value)} />
+                    </div>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-center">
+                { content.map((res) => {
+
+                    return (
+                        <div  className={selectedId == res.id ? 'border-img col-lg-2 col-md-6 mb-4' : 'col-lg-2 col-md-6 mb-4'} key={res.id} onClick={() => { handleChooseItem(res.id)}} >
+                            <img className="card-img-top" src={res.download_url} alt="Card image cap" />
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <div className="d-flex justify-content-between">
+                <button type="button" className="btn btn-warning" onClick={handleSkip}>Skip</button>
+
+                <button type="button" className="btn btn-primary" onClick={handleSave}>Save</button>
+
+            </div>
+        </>
+
     );
 };
 
--- src/services/image.service.js
+++ src/services/image.service.js
@@ -3,8 +3,9 @@ import authHeader from "./auth-header";
 
 const API_URL = "https://picsum.photos/v2/";
 
-const getImages = () => {
-    return axios.get(API_URL + "list?page=1&limit=10");
+const getImages = (params = {}) => {
+    const query = new URLSearchParams({...params, ...{page: 1, limit: 10}}).toString()
+    return axios.get(API_URL + "list?"+ query);
 };
 
 export default {
