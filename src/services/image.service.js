a232e5c0d9e9360abbf05554a24b61616c27c2fa
tuan <anhtuan3996@gmail.com>
2023-01-04 00:01:57 +0700

    update api

--- src/services/image.service.js
+++ src/services/image.service.js
@@ -5,7 +5,16 @@ const API_URL = process.env.REACT_APP_API_URL
 
 const getImages = (params = {}) => {
     const query = new URLSearchParams({...params, ...{page: 1, limit: 10}}).toString()
-    return axios.get(API_URL + "/items?"+ query);
+
+    const config = {
+        method: 'get',
+        url: API_URL + "/items?"+ query,
+        headers: {
+            'Authorization': 'TOKEN d0d6387204d47cd756d0b33f22858df7945fbb09'
+        }
+    };
+
+    return axios(config);
 };
 
 export default {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,7 +4,15 @@ import authHeader from "./auth-header";
 const API_URL = process.env.REACT_APP_API_URL
 
 const getPublicContent = () => {
-  return axios.get(API_URL + "/users/get-details");
+  const config = {
+    method: 'get',
+    url: API_URL + "/users/get-details",
+    headers: {
+      'Authorization': 'TOKEN d0d6387204d47cd756d0b33f22858df7945fbb09'
+    }
+  };
+
+  return axios(config);
 };
 
 export default {
